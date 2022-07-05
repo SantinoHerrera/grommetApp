ad0e507cdc3db97bac9e1a5fb497ddc011fc2df3
santino herrera cano <herreracanosantino@gmail.com>
2022-07-04 22:33:30 -0500

    grommet setup is done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grommet } from 'grommet';
+import AppRouter from './AppRouter';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <AppRouter></AppRouter>
+    </Grommet>
   );
 }
 
